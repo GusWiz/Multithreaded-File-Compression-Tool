b3d3c17799b384206f6350dea06b14e95de85fb3
Gustavo <pow10@txstate.edu>
2025-07-09 16:25:17 -0500

    Plan for a new function that will be used for the purposes of the program

--- include/file_io.hpp
+++ include/file_io.hpp
@@ -6,3 +6,9 @@
 std::ifstream openFile(const std::string& fileName);
 void closeFile(const std::ifstream& file);
 std::streamsize getFileSize(const std::ifstream& file);
+
+// create a function that stops multi-threading issues, like
+// race condition
+// Approach to this function will be chunking the given file
+// Allowing us to track have our threads deal with its own given chunks
+// while keeping progress (estimate progress (percentage), track completeness)
